026b1922cecc24bb5d36afe4d727c25f3e7edbd8
Maximilian Moehl <maximilian.moehl@gmail.com>
2023-03-10 15:43:17 +0100

    style(nix): format file

--- .nix/flake.nix
+++ .nix/flake.nix
@@ -1,13 +1,12 @@
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        formatter = pkgs.nixfmt;
         packages.default = pkgs.buildEnv {
           name = "default";
           paths = with pkgs; [
@@ -44,7 +43,6 @@
             yq-go
           ];
         };
-      }
-    );
+      });
 }
 
