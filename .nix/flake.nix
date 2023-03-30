88e81e2f888d8731a61316ea81339fed82d186b9
Maximilian Moehl <maximilian@moehl.eu>
2023-03-30 13:39:56 +0200

    update

--- .nix/flake.nix
+++ .nix/flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
@@ -10,6 +10,7 @@
         packages.default = pkgs.buildEnv {
           name = "default";
           paths = with pkgs; [
+            alacritty
             bat
             borgbackup
             btop
@@ -43,6 +44,7 @@
             unixtools.watch
             vault
             yq-go
+            yubikey-manager
           ];
         };
       });
